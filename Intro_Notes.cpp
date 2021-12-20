ea71dc4a85c3a880f0bc6d579bc6892eab86bca9
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-12-20 10:58:06 -0500

    Create Intro_Notes.cpp

---
+++ Intro_Notes.cpp
@@ -0,0 +1,33 @@
+// CS2 Day 1 Notes: This file contains the 'main' function. Program execution begins and ends there.
+
+/*
+   I CAN perform input and output using cin and cout
+   I CAN create and use variables including int, double, and string
+   I CAN use basic math computations to solve problems
+   I CAN explain the purpose of a semicolon in C++
+*/
+
+#include <iostream>
+
+int main()
+{
+    //COUT means Console OUTput
+    std::cout << "Hello World!\n";
+
+    //Changing the console color
+    
+    //1 = blue background, F = white text
+
+    //Making variables
+
+    //Simple computation and output
+
+    //Semi-colons are like the periods at the end of a sentence.
+    //This frees us from having exactly one statement on every line.
+
+    //CIN means Console INput 
+
+    //Code will pause until user enters two values
+
+
+}
